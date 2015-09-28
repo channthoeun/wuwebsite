b53912f637d51173a9eef0421daa697eb26039bd
Channthoeun Ken <channthoeun@gmail.com>
2015-09-29 01:20:17 +0700

    Initital commit

---
+++ UsingGit04/UsingGit04/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace UsingGit04.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ UsingGit04/UsingGit04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UsingGit04.Global" Language="C#" %>
